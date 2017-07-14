256e4c41187535aec20abcf69f7910dac4bf61ce
Colton Mathews <mathews.colton@gmail.com>
2017-07-14 15:57:52 -0600

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,36 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_items = Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(portfolio_params)
+    
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path notice: 'Portfolio was successfully posted.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_items = Portfolio.destroy
+  end 
+
+  def update
+    @portfolio_items = Portfolio.update
+  end
+
+  def edit
+    
+  end
+
+      def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
