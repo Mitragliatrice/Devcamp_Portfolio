47b881181293d2d8a08d1ce8be7a8410c957a1c3
Colton Mathews <mathews.colton@gmail.com>
2017-07-14 17:08:09 -0600

    Portfolio Updates

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,17 +19,27 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+
   def destroy
     @portfolio_items = Portfolio.destroy
   end 
 
   def update
-    @portfolio_items = Portfolio.update
+    @portfolio_items = Portfolio.find(params[:id])
+
+     respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path notice: 'Portfolio was successfully posted.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
-  def edit
-    
-  end
+
 
       def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
