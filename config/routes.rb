abcc8a720a9e2ed8fd485780eb277b2eafc803ee
Colton Mathews <mathews.colton@gmail.com>
2017-07-18 13:26:14 -0600

    finished portfolio feature added draft/published toggle.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact-me', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
