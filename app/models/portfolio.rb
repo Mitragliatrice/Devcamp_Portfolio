82935c29dc324336e67b1c349f1fb47ab3ecaa3d
Colton Mathews <mathews.colton@gmail.com>
2017-07-19 11:10:46 -0600

    Ipmlimented defaults for port. image items.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,19 @@
 class Portfolio < ApplicationRecord
-
+after_initialize :set_default
 validates_presence_of :title, :body, :main_img, :thumb_img
 
   def self.angular
       where(subtitle: 'Angular')
   end
 
-    def self.ruby_on_rails
+  def self.ruby_on_rails
       where(subtitle: "Ruby on Rails")
+  end
+
+  after_initialize :set_default
+  def set_default (height:, width:)
+    self.main_img ||= "http://via.placeholder.com/"height:"x"width:""
+    self.thumb_img ||= "http://via.placeholder.com/"height:"x"width:""
   end
 
 end
