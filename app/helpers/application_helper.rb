199b265272e27e29300d352d6f02b5ee3ddc0d69
Colton Mathews <mathews.colton@gmail.com>
2017-07-27 14:35:48 -0600

    fixed routing bug and added map to contact page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path, class: style)
     else 
      (link_to current_user.name, edit_user_registration_path, class: style) + 
-     "<br>".html_safe +
+     " ".html_safe +
      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     end 
   end
