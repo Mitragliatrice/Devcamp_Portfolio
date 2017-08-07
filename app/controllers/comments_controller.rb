ba2b5feed5b0066faa0b20e3d2aedd63b0dacd3a
Colton Mathews <mathews.colton@gmail.com>
2017-08-07 10:49:55 -0600

    Created show and create for commments on our Blog Show page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  def
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
